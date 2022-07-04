3c754b7e6cea9f2fd69cb2f209d416831ec23e3e
Gisela <89365957+giselanm@users.noreply.github.com>
2022-07-04 16:33:31 -0300

    Creacion de banner ,rsociales , logoArgpro

--- ProyectoIntegradorGiselaMartinez/src/app/app.module.ts
+++ ProyectoIntegradorGiselaMartinez/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { RsocialesComponent } from './components/rsociales/rsociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    RsocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
