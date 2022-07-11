8ccd6887c3dc82942d288f86d4f2ebdcc88d4ee0
Gisela <89365957+giselanm@users.noreply.github.com>
2022-07-11 10:55:21 -0300

    Creacion de Acercade y Foto de Perfil

--- ProyectoIntegradorGiselaMartinez/src/app/app.module.ts
+++ ProyectoIntegradorGiselaMartinez/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
 import { RsocialesComponent } from './components/rsociales/rsociales.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoArgProgComponent,
     RsocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
