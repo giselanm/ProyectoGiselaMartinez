5a6ce9171c64f702671362b9ab32fe3c4779a992
Gisela <89365957+giselanm@users.noreply.github.com>
2022-07-17 15:35:44 -0300

    Creacion  footer

--- ProyectoIntegradorGiselaMartinez/src/app/app.module.ts
+++ ProyectoIntegradorGiselaMartinez/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
-import { RsocialesComponent } from './components/rsociales/rsociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -13,18 +12,21 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { FormsModule } from '@angular/forms';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgProgComponent,
-    RsocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- ProyectoIntegradorGiselaMartinez/src/app/components/footer/footer.component.spec.ts
+++ ProyectoIntegradorGiselaMartinez/src/app/components/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RsocialesComponent } from './rsociales.component';
+import { FooterComponent } from './footer.component';
 
-describe('RsocialesComponent', () => {
-  let component: RsocialesComponent;
-  let fixture: ComponentFixture<RsocialesComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RsocialesComponent ]
+      declarations: [ FooterComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(RsocialesComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
